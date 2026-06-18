47ac6dfe5f92af9dab2cf16b0964d331993a0160
AnshikaBansal2004 <anshikabansal917@gmail.com>
2026-06-19 00:10:23 +0530

    creating JWT token and storing in session cookie

---
+++ authentication/utils/generateJWTtoken.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+//adds the jwt in cookie for this response
+const generateJWTtokenandsetCookie = (userID,res)=>{
+      const token = jwt.sign({userID},process.env.JWTTokenSecret,{expiresIn: "25d"});
+      res.cookie("jtw",token,{
+            maxAge : 15*24*60*60*1000,
+            httpOnly : true,
+            sameSite : 'strict',
+            secure : false
+      })
+}
+
+export default generateJWTtokenandsetCookie

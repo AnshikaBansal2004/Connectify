d9a8d194998f3d81cdfbc66eae4b349ca6963dd6
AnshikaBansal2004 <anshikabansal917@gmail.com>
2026-06-19 12:21:24 +0530

    saving the cookies in the browser

--- authentication/index.js
+++ authentication/index.js
@@ -10,7 +10,11 @@ const port = process.env.PORT || 5000;
 
 const app = express(); //application
 //MIDDLEWARE:
-app.use(cors());
+app.use(cors(
+      {credentials : true,
+       origin : "http://localhost:3000"
+      }
+));
 app.use(express.json()); //parses the incoming request bodyand populates req.body with the JSON format data
 
 app.use('/auth',authRouter); //any req whose path starts with /auth will be routed here
